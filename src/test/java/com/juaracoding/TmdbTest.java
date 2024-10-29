98da8e763c30a12a642338a294acf9260c489bb3
raisya20 <raisyaa95@gmail.com>
2024-10-29 17:30:51 +0700

    Add new test scenario

--- src/test/java/com/juaracoding/TmdbTest.java
+++ src/test/java/com/juaracoding/TmdbTest.java
@@ -28,9 +28,24 @@ public class TmdbTest {
 
         response.then()
                 .statusCode(200)
-                .body("results[0].title", equalTo("Venom: The Last Dance")); // Validasi judul film yang diharapkan
+                .body("results[0].title", equalTo("Venom: The Last Dance")); // Title Validation
         System.out.println("Now Playing Movie Title: " + response.jsonPath().getString("results[0].title"));
     }
+
+    // Negative test: Invalid Now Playing Page
+    @Test
+    public void testGetMovieNowPlayingInvalidPage() {
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .queryParam("page", 999999) // Invalid Page
+                .get("/movie/popular");
+
+        response.then()
+                .statusCode(400); // Bad Request
+        System.out.println("Invalid Page Now Playing, Status Code: " + response.getStatusCode());
+    }
+
 
     // Scenario Test 2: Get Movie Popular
     @Test
@@ -43,22 +58,22 @@ public class TmdbTest {
 
         response.then()
                 .statusCode(200)
-                .body("results[0].title", equalTo("The Wild Robot")); // Validasi judul film yang diharapkan
+                .body("results[0].title", equalTo("The Wild Robot")); // Title Validation
         System.out.println("Popular Movie Title: " + response.jsonPath().getString("results[0].title"));
     }
 
-    // Negative test : Halaman negatif
+    // Negative test: Negative Popular Page
     @Test
     public void testGetMoviePopularInvalidPage() {
         Response response = given()
                 .header("Authorization", myToken)
                 .queryParam("language", "en-US")
-                .queryParam("page", -1) // Halaman negatif untuk kasus negatif
+                .queryParam("page", -1) // Negative page for Negative Test Scenario
                 .get("/movie/popular");
 
         response.then()
-                .statusCode(400); // Mengharapkan Bad Request
-        System.out.println("Invalid Page Test Status Code: " + response.getStatusCode());
+                .statusCode(400); // Bad Request
+        System.out.println("Invalid Page Popular, Status Code: " + response.getStatusCode());
     }
 
     // Scenario Test 3: Post Rating
@@ -73,38 +88,111 @@ public class TmdbTest {
 
         response.then()
                 .statusCode(201)
-                .body("status_message", equalTo("Success.")); // Validasi pesan sukses
+                .body("status_message", equalTo("Success.")); // Validate Succes Message
         System.out.println("Post Rating Response: " + response.jsonPath().getString("status_message"));
     }
 
-    // Negative test : Post Rating >10
+    // Negative test: Post Rating >10
     @Test
     public void testPostInvalidMovieRating() {
-        int movieId = 123; // Replace with a valid movie ID
+        int movieId = 123; // Valid movie ID
         Response response = given()
                 .header("Authorization", myToken)
                 .contentType("application/json")
-                .body("{\"value\": 15}") // Nilai rating di luar batas (1-10)
+                .body("{\"value\": 15}") // Rate >10
                 .post("/movie/" + movieId + "/rating");
 
         response.then()
-                .statusCode(400); // Mengharapkan Bad Request
+                .statusCode(400); // Bad Request
         System.out.println("Invalid Rating Response Status Code: " + response.getStatusCode());
     }
 
     // Scenario Test 4: Get Movie Details
     @Test
     public void testGetMovieDetails() {
-        int movieId = 550; // movie ID untuk "Fight Club"
+        int movieId = 550; // Movie Id "Fight Club"
         Response response = given()
                 .header("Authorization", myToken)
-                .queryParam("language", "en-US") // Menambahkan query parameter 'language'
+                .queryParam("language", "en-US")
                 .get("/movie/" + movieId);
 
         response.then()
-                .statusCode(200) // Memastikan status code 200 OK
-                .body("title", notNullValue()) // Verifikasi bahwa respons berisi movie details
-                .body("title", equalTo("Fight Club")); // Memastikan bahwa movie memiliki informasi "title movie"
+                .statusCode(200) // Verify Status Code : 200
+                .body("title", notNullValue()) // Verify Response Get Movie Details
+                .body("title", equalTo("Fight Club")); // Verify Movie Had "title movie"
         System.out.println("Movie Details Title: " + response.jsonPath().getString("title"));
     }
+
+
+    // Negative test: Get Movie Details Invalid Id
+    @Test
+    public void testGetMovieDetailsNotFound() {
+        int invalidMovieId = 99999999; // Invalid Id
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .get("/movie/" + invalidMovieId);
+        response.then()
+                .statusCode(404); // code Not Found
+        System.out.println("Invalid Id Status Code: " + response.getStatusCode());
+    }
+
+    // Scenario Test 5: Get Movie Top Rated
+    @Test
+    public void testGetMovieTopRated() {
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .queryParam("page", 1)
+                .get("/movie/top_rated");
+
+        response.then()
+                .statusCode(200)
+                .body("results[0].title", equalTo("The Shawshank Redemption")); // Title Validation
+        System.out.println("Top Rated Movie Title: " + response.jsonPath().getString("results[0].title"));
+    }
+
+    // Negative test: Negative Top Rated Page
+    @Test
+    public void testGetMovieTopRatedInvalidPage() {
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .queryParam("page", 99999) // Negative page for Negative Test Scenario
+                .get("/movie/top_rated");
+
+        response.then()
+                .statusCode(400); // Bad Request
+        System.out.println("Invalid Page Top Rated, Status Code: " + response.getStatusCode());
+    }
+
+    // Scenario Test 6: Get Movie Upcoming
+    @Test
+    public void testGetMovieUpcoming() {
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .queryParam("page", 1)
+                .get("/movie/upcoming");
+
+        response.then()
+                .statusCode(200)
+                .body("results[0].title", equalTo("The Wild Robot")); // Title Validation
+        System.out.println("Upcoming Movie Title: " + response.jsonPath().getString("results[0].title"));
+    }
+
+    // Negative test: Negative Upcoming Page
+    @Test
+    public void testGetMovieUpcomingPage() {
+        Response response = given()
+                .header("Authorization", myToken)
+                .queryParam("language", "en-US")
+                .queryParam("page", 99999) // Negative page for Negative Test Scenario
+                .get("/movie/upcoming");
+
+        response.then()
+                .statusCode(400); // Bad Request
+        System.out.println("Invalid Page Upcoming, Status Code: " + response.getStatusCode());
+    }
+
 }
